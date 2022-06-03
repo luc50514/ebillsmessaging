e4c87621620a4f33dda9242f96bb105718f4931a
luc50514@gmail.com <luc50514@gmail.com>
2022-06-03 19:03:27 -0400

    first commit

---
+++ lib/ebillsmessaging.ex
@@ -0,0 +1,18 @@
+defmodule Ebillsmessaging do
+  @moduledoc """
+  Documentation for `Ebillsmessaging`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ebillsmessaging.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ebillsmessaging_test.exs
@@ -0,0 +1,8 @@
+defmodule EbillsmessagingTest do
+  use ExUnit.Case
+  doctest Ebillsmessaging
+
+  test "greets the world" do
+    assert Ebillsmessaging.hello() == :world
+  end
+end
