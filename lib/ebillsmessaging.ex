38cf57c84f08008e413bd0d99daeb4811401d93b
luc50514@gmail.com <luc50514@gmail.com>
2022-06-03 21:57:56 -0400

    added passing test

--- lib/ebillsmessaging.ex
+++ lib/ebillsmessaging.ex
@@ -15,4 +15,18 @@ defmodule Ebillsmessaging do
   def hello do
     :world
   end
+  def start_link do
+    Task.start_link(fn -> loop(%{}) end)
+  end
+
+  defp loop(map) do
+    receive do
+      {:get, key, caller} ->
+        send caller, Map.get(map, key)
+        loop(map)
+      {:put, key, value} ->
+        loop(Map.put(map, key, value))
+    end
+
+  end
 end
--- test/ebillsmessaging_test.exs
+++ test/ebillsmessaging_test.exs
@@ -5,4 +5,16 @@ defmodule EbillsmessagingTest do
   test "greets the world" do
     assert Ebillsmessaging.hello() == :world
   end
+  test "recieves pid when start link is started" do
+    {:ok, pid} = Ebillsmessaging.start_link()
+    assert pid != nil
+  end
+  test "recieves message when put is sent" do
+    {:ok, pid} = Ebillsmessaging.start_link()
+    Process.register(pid, :ebills)
+    send(:ebills, {:put, :hello, :world})
+    send(:ebills, {:get, :hello, self()})
+    assert_receive :world
+  end
+
 end
